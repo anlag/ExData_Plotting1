0fc8c5360a1fd718759d3068b58720c9428b000b
Laanders <laantr@gmail.com>
2015-02-08 18:41:44 +0100

    Fixed up comments in plot4.R

--- plot4.R
+++ plot4.R
@@ -1,4 +1,4 @@
-# this script reads data, processes it, and draws plot 3: a multiline plot of sub metered power over time
+# this script reads data, processes it, and draws plot 4: a 2x2 multiple pane figure with mixed content
 
 # read data frame from file
 inData <- read.csv("household_power_consumption.txt", sep = ";", colClasses = "character", na.strings = "?")
@@ -9,7 +9,7 @@ twoDays <- inData[inData$Date == "1/2/2007" | inData$Date == "2/2/2007", ]
 # create a new DateTime column containing both date and time in the POSIXlt class
 twoDays$DateTime <- strptime(paste(twoDays$Date, twoDays$Time), format="%d/%m/%Y %H:%M:%S")
 
-# convert the sub metering columns to numeric format
+# convert the relevant columns to numeric format
 twoDays$Global_active_power <- as.numeric(twoDays$Global_active_power)
 twoDays$Voltage <- as.numeric(twoDays$Voltage)
 twoDays$Sub_metering_1 <- as.numeric(twoDays$Sub_metering_1)
@@ -20,16 +20,16 @@ twoDays$Global_reactive_power <- as.numeric(twoDays$Global_reactive_power)
 # open the png device
 png(file = "plot4.png")
 
-# make a 2x2 multiplot figure
+# make a 2x2 multiplot figure, filled by row
 par(mfrow = c(2, 2))
 
-# draw the first plot
+# draw the first plot (top left)
 plot(twoDays$DateTime, twoDays$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power", main = "")
 
-# draw the second plot
+# draw the second plot (top right)
 plot(twoDays$DateTime, twoDays$Voltage, type = "l", xlab = "datetime", ylab = "Voltage", main = "")
 
-# draw the third plot
+# draw the third plot (bottom left)
 plot(twoDays$DateTime, twoDays$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering", main = "")
 lines(twoDays$DateTime, twoDays$Sub_metering_1, col="black")
 lines(twoDays$DateTime, twoDays$Sub_metering_2, col="red")
@@ -37,7 +37,7 @@ lines(twoDays$DateTime, twoDays$Sub_metering_3, col="blue")
 
 legend("topright", bty = "n", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
-# draw the fourth plot
+# draw the fourth plot (bottom right)
 plot(twoDays$DateTime, twoDays$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power", main = "")
 
 # close the file device
